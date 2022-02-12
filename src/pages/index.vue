cd9a8df22f03c4956adebfc688e4da9ab09b4976
Selemon <77731608+selemonjr@users.noreply.github.com>
2022-02-12 20:50:46 +0300

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <c-center p="10" bg-gradient="linear(to-l, emerald.500, teal.500)" color="white" font-weight="bold" h="100vh">
+    <c-v-stack spacing="8">
+      <c-heading size="4xl" color="white">
+        Chakra UI Vue v1 ⚡️
+      </c-heading>
+      <c-stack spacing="8" :direction="{ base: 'column', sm: 'column', md: 'row' }" color="emerald.500">
+        <c-h-stack>
+          <c-button size="md" as="router-link" left-icon="pie-chart" to="/dashboard/settings">
+            Dashboard
+          </c-button>
+          <c-button size="md" as="router-link" left-icon="user" to="/dashboard/accounts">
+            Accounts
+          </c-button>
+        </c-h-stack>
+        <c-button align-self="start" :_hover="{ bg: 'gray.50', color: 'emerald.500' }" size="md" as="a" left-icon="book-open" href="https://next.vue.chakra-ui.com" target="_blank" variant="outline" color="gray.50" color-scheme="emerald">
+          Docs
+        </c-button>
+      </c-stack>
+      <c-button :_hover="{ bg: 'gray.50', color: 'emerald.500' }" align-self="start" size="sm" as="a" left-icon="github" href="https://github.com/codebender828/chakra-ui-chidori-starter" target="_blank" variant="outline" color="gray.50" color-scheme="emerald">
+        View on Github
+      </c-button>
+    </c-v-stack>
+  </c-center>
+</template>
---
+++ src/utils/icons.ts
@@ -0,0 +1,19 @@
+export {
+  feBook,
+  feMessageCircle,
+  fePackage,
+  feSearch,
+  feStar,
+  feCompass,
+  feExternalLink,
+  feArrowLeft,
+  fePieChart,
+  feUser,
+  feBookOpen,
+  feActivity,
+  feSettings,
+  feHome,
+  feGithub,
+  feMenu,
+  feX
+} from 'feather-icons-paths'
